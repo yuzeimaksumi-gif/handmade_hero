dbd4e4ed4f42f7b3afe499056629e8974059618f
unknown <yuzeidutta01@gmail.com>
2026-09-26 13:29:18 +0530

    for sum from 1 to n

---
+++ subham_dutta/sum1ton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int s,i,n;
+    printf("the ending range:\n");
+    scanf("%d",n);
+    s=0;
+    for(i=0;i<=n;i++)
+    s=s+i;
+    printf("the sum is %d",s);
+    return 0;
+}
+
+    
